cc7d70b288c71ae072d8b4af196b055a8675f33f
Cesar <cesarjcontreras30@gmail.com>
2024-07-19 23:20:42 -0500

    refactor(users): changing the application functions into a class

--- src/pages/users/update/application/users-update.application.ts
+++ src/pages/users/update/application/users-update.application.ts
@@ -7,8 +7,6 @@ import {
   createPopUpSimple,
 } from "../../../../utils/pop-up.util";
 
-import { UserResponseDto } from "../../../../dtos/user-response.dto";
-import { UserUpdateDto } from "../../../../dtos/user-update.dto";
 import { usersService } from "../../../../services/users.service";
 import { fromUserFormEntityToUserUpdateDtoAdapter } from "../../adapters/from-user-form-entity-to-user-update-dto.adapter";
 import { fromUserResponseDtoToUserFormEntityAdapter } from "../../adapters/from-user-response-dto-to-user-form-entity.adapter";
@@ -16,33 +14,7 @@ import { UserFormEntity } from "../entities/user-form.entity";
 import { UserFormPasswordEntity } from "../entities/user-form-password.entity";
 import { UserUpdatePasswordDto } from "../../../../dtos/user-update-password.dto";
 import { fromUserFormPasswordEntityToUserUpdatePasswordDtoAdapter } from "../../adapters/from-user-form-password-entity-to-user-update-password-dto.adapter";
-
-export const fillUserForm = async (
-  id: number,
-  reset: UseFormReset<UserFormEntity>,
-  callEndpoint: (
-    axiosCall: AxiosCall<UserResponseDto>
-  ) => Promise<AxiosResponse<UserResponseDto, any>>
-) => {
-  const service = usersService.getById(id);
-  const response = await callEndpoint(service);
-  reset(fromUserResponseDtoToUserFormEntityAdapter(response.data));
-};
-
-export const updateUser = async (
-  id: number,
-  data: UserFormEntity,
-  callEndpoint: (
-    axiosCall: AxiosCall<UserUpdateDto>
-  ) => Promise<AxiosResponse<any, any>>
-) => {
-  const service = usersService.update(
-    id,
-    fromUserFormEntityToUserUpdateDtoAdapter(data)
-  );
-  await callEndpoint(service);
-  createPopUpSimple("El usuario actualizado");
-};
+import { LoggerUtil } from "../../../../utils/logger.util";
 
 export const updateUserPassword = async (
   id: number,
@@ -67,3 +39,36 @@ export const updateUserPassword = async (
   createPopUpSimple("La contraseña ha sido cambiada");
   reset();
 };
+
+export class UserUpdateFormApplication {
+  constructor(
+    private userId: number,
+    private reset: UseFormReset<UserFormEntity>,
+    private callEndpoint: (
+      axiosCall: AxiosCall<any>
+    ) => Promise<AxiosResponse<any, any>>
+  ) {}
+
+  async fillUserForm() {
+    try {
+      const service = usersService.getById(this.userId);
+      const response = await this.callEndpoint(service);
+      this.reset(fromUserResponseDtoToUserFormEntityAdapter(response.data));
+    } catch (error) {
+      LoggerUtil.logError(error);
+    }
+  }
+
+  async handleSubmit(data: UserFormEntity) {
+    try {
+      const service = usersService.update(
+        this.userId,
+        fromUserFormEntityToUserUpdateDtoAdapter(data)
+      );
+      await this.callEndpoint(service);
+      createPopUpSimple("El usuario actualizado");
+    } catch (error) {
+      LoggerUtil.logError(error);
+    }
+  }
+}
--- src/pages/users/update/components/UsersUpdateForm/UsersUpdateForm.tsx
+++ src/pages/users/update/components/UsersUpdateForm/UsersUpdateForm.tsx
@@ -1,18 +1,16 @@
-import { useContext, useMemo } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { useContext, useEffect, useMemo } from "react";
+import { useForm } from "react-hook-form";
 
 import { useUserSelector } from "../../../../../redux/user.hooks";
 import { Button } from "../../../../../components/Button";
 import { FormContainer } from "../../../../../components/Form/FormContainer";
 import useFetchAndLoad from "../../../../../hooks/useFetchAndLoad";
 import { LoginResponseEntity } from "../../../../../entities";
-import { LoggerUtil } from "../../../../../utils/logger.util";
 
 import { UserFormEntity } from "../../entities/user-form.entity";
 import { UsersUpdateContext } from "../../context/UsersUpdate.context";
-import { updateUser } from "../../application/users-update.application";
+import { UserUpdateFormApplication } from "../../application/users-update.application";
 import { getUsersUpdateFormFields } from "./users-update-form-field";
-import { useFillUserForm } from "../../hooks/useFillUserForm";
 
 export const UsersUpdateForm = () => {
   const { setLoading } = useContext(UsersUpdateContext);
@@ -23,21 +21,21 @@ export const UsersUpdateForm = () => {
   const { handleSubmit, register, reset } = useForm<UserFormEntity>();
   const { callEndpoint } = useFetchAndLoad(setLoading);
 
-  const onSubmit: SubmitHandler<UserFormEntity> = (data) => {
-    try {
-      updateUser(user.id, data, callEndpoint);
-    } catch (error) {
-      LoggerUtil.logError(error);
-    }
-  };
+  const application = new UserUpdateFormApplication(
+    user.id,
+    reset,
+    callEndpoint
+  );
 
-  useFillUserForm({ reset, callEndpoint });
+  useEffect(()=>{
+    application.fillUserForm();
+  },[])
 
   return (
     <div>
       <h2>Datos generales</h2>
       <FormContainer
-        onSubmit={handleSubmit(onSubmit)}
+        onSubmit={handleSubmit(application.handleSubmit)}
         fields={useMemo(() => getUsersUpdateFormFields(register), [])}
       >
         <Button text="Guardar" type="submit" />
