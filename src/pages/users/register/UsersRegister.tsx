64b8c743b86d0b48e176f893b2798a5486966f32
Cesar <cesarjcontreras30@gmail.com>
2024-07-20 14:05:45 -0500

    style(users-register): centering the form and adding link to login

--- src/pages/users/register/UsersRegister.tsx
+++ src/pages/users/register/UsersRegister.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { UsersRegisterContext } from "./context/UsersRegister.context";
 import { UsersRegisterForm } from "./components/UsersRegisterForm/UsersRegisterForm";
+import { Typography } from "@mui/material";
+import { CustomeLink } from "../../../components/CustomeLink";
+import { routes } from "../../../variables/routes.variables";
 
 export const UsersRegister = () => {
   const [loading, setLoading] = useState(false);
@@ -11,7 +14,20 @@ export const UsersRegister = () => {
         setLoading,
       }}
     >
-      <UsersRegisterForm />
+      <div style={{ marginTop: "25px" }}>
+        <div style={{ maxWidth: "800px", margin: "0 auto" }}>
+          <div className="div">
+            <Typography component="h1" variant="h4" textAlign={"center"}>
+              Registro
+            </Typography>
+          </div>
+          <UsersRegisterForm />
+          <CustomeLink
+            text="¿Ya tienes una cuenta? Presiona aquí"
+            to={routes.login}
+          />
+        </div>
+      </div>
     </UsersRegisterContext.Provider>
   );
 };
