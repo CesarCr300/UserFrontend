cfc672d8bae3a2940e6b7b58f17e24addf256111
Cesar <cesarjcontreras30@gmail.com>
2024-07-19 23:45:17 -0500

    refactor(login): creating application class

--- src/pages/login/application/login.application.ts
+++ src/pages/login/application/login.application.ts
@@ -9,6 +9,7 @@ import { AxiosResponse } from "axios";
 import { createLoginResponseDtoToEntityAdapter } from "../adapters/create-login-response-to-user.adapter";
 import { defaultAuthenticatedUserRoute } from "../../../variables/routes.variables";
 import { LoginResponseDto } from "../dtos/login-response.dto";
+import { LoggerUtil } from "../../../utils/logger.util";
 
 export const login = async (
   email: string,
@@ -26,3 +27,28 @@ export const login = async (
   dispatch(loginReducer(createLoginResponseDtoToEntityAdapter(response.data)));
   navigate(defaultAuthenticatedUserRoute);
 };
+
+export class LoginApplication {
+  constructor(
+    private dispatch: Dispatch<Action>,
+    private loginReducer: ActionCreatorWithPayload<LoginResponseEntity>,
+    private navigate: NavigateFunction,
+    private callEndpoint: (
+      axiosCall: AxiosCall<LoginResponseDto>
+    ) => Promise<AxiosResponse<any, any>>
+  ) {}
+
+  async login(email: string, password: string) {
+    try {
+      const response = await this.callEndpoint(
+        loginService(createLoginAdapter(email, password))
+      );
+      this.dispatch(
+        this.loginReducer(createLoginResponseDtoToEntityAdapter(response.data))
+      );
+      this.navigate(defaultAuthenticatedUserRoute);
+    } catch (error) {
+      LoggerUtil.logError(error);
+    }
+  }
+}
--- src/pages/login/components/LoginForm.tsx
+++ src/pages/login/components/LoginForm.tsx
@@ -1,15 +1,16 @@
+import { useContext } from "react";
 import { Box, FormControl, TextField, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 import { useUserDispatch } from "../../../redux/user.hooks";
 import useFetchAndLoad from "../../../hooks/useFetchAndLoad";
 import { Button } from "../../../components/Button";
 import { login as loginReducer } from "../../../redux/user.slice";
-import { LoginEntity } from "../entities/login.entity";
-import { login } from "../application/login.application";
-import { useContext } from "react";
 import { LoginContext } from "../context/login.context";
+
+import { LoginEntity } from "../entities/login.entity";
+import { LoginApplication } from "../application/login.application";
 
 export const LoginForm = () => {
   const { setLoading } = useContext(LoginContext);
@@ -18,16 +19,13 @@ export const LoginForm = () => {
   const dispatch = useUserDispatch();
   const navigate = useNavigate();
 
-  const onSubmit: SubmitHandler<LoginEntity> = (data) => {
-    login(
-      data.email,
-      data.password,
-      dispatch,
-      loginReducer,
-      navigate,
-      callEndpoint
-    );
-  };
+  const application = new LoginApplication(
+    dispatch,
+    loginReducer,
+    navigate,
+    callEndpoint
+  );
+
   return (
     <Box
       display="flex"
@@ -40,7 +38,11 @@ export const LoginForm = () => {
       }}
       boxShadow={10}
     >
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form
+        onSubmit={handleSubmit((d) => {
+          application.login(d.email, d.password);
+        })}
+      >
         <Typography component="h1" variant="h4" textAlign={"center"}>
           Inicio de sesión
         </Typography>
